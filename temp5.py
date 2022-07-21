77a5ccb73f2ccd7e66e9852444509ac8f25cd52b
cswnote <cswnote@gmail.com>
2022-07-21 16:56:55 +0900

    from M1 Macbook Air

--- temp5.py
+++ temp5.py
@@ -1,11 +1,40 @@
+# import os
+# import sys
+# import pyautogui as pag
+# import time
+# from datetime import datetime
+# import keyboard
+#
+# while True:
+#     x, y = pag.position()
+#     print(x, y)
+#     time.sleep(2)
+
+
 import os
 import sys
-import pyautogui as pag
-import time
-from datetime import datetime
-import keyboard
+import shutil
 
-while True:
-    x, y = pag.position()
-    print(x, y)
-    time.sleep(2)
+path = '/Users/rainyseason/winston/Workspace/python/Pycharm Project/sinewave_analyze/Evaluation/tek_csv/'
+
+files = os.listdir(path)
+
+files = [file for file in files if file[:3] == 'tek']
+
+files.sort()
+
+for file in files:
+    num = int(file[3:-4])
+    if 8786 <= num <= 8962:
+        num  = num - 13
+        src = path + file
+        extention = file[-3:]
+        file = file[0:4] + str(num) + '.' + extention
+        if extention == 'png':
+            dst = '/Users/rainyseason/winston/Workspace/python/Pycharm Project/sinewave_analyze/Evaluation/tek_png/' + file
+        elif extention == 'csv':
+            dst = '/Users/rainyseason/winston/Workspace/python/Pycharm Project/sinewave_analyze/Evaluation/tek_csv/' + file
+        else:
+            dst = '/Users/rainyseason/winston/Workspace/python/Pycharm Project/sinewave_analyze/Evaluation/tek_set/' + file
+
+        shutil.copy(src, dst)
